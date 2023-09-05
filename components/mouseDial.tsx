e0c7fb591872a9d3801431be1eb4b51725b122cb
Tao <taole.chen@gmail.com>
2023-09-05 22:36:00 +0200

    dial working. trying to add category banners

--- components/mouseDial.tsx
+++ components/mouseDial.tsx
@@ -5,51 +5,90 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 function MouseDial() {
-
-    const centerX = Math.round(window.innerWidth / 2);
+  const centerX = Math.round( window.innerWidth / 2);
+  const lineWidthThreshold = 150;
+  const rotationThreshold = 200;
   const [x, setX] = useState(centerX);
   const [y, setY] = useState(Math.round(window.innerHeight / 2));
 
   const handleMouse = (e: React.MouseEvent<HTMLImageElement>): void => {
     setX(e.pageX);
     setY(e.pageY);
-    };
-    
-    const distanceToCenter = () :number => {
-        return centerX - x;
-    }
+  };
 
-    const translateRotation = () : number => {
-        return map(-1*distanceToCenter(), 0, centerX, 0, 90);
-    };
-    
-    const translateWidth = (): number => {
-        return map(Math.abs(distanceToCenter()), 2, centerX, 0, window.innerHeight+100);        
-    }
+  const distanceToCenter_signed = (): number => {
+    return centerX - x;
+  };
+
+  const translateRotation = (): number => {
+    if (Math.abs(distanceToCenter_signed()) < rotationThreshold) return 0;
+
+    const val = map(
+      Math.abs(distanceToCenter_signed()),
+      rotationThreshold,
+      centerX,
+      0,
+      100
+    );
+    const sign =
+      (distanceToCenter_signed() * -1) / Math.abs(distanceToCenter_signed());
+    // angle caps at 90
+    return val > 90 || val < -90 ? 90 * sign : val * sign;
+  };
+
+  const translateWidth = (): number => {
+    const dist_unsigned = Math.abs(distanceToCenter_signed());
+    if (dist_unsigned < lineWidthThreshold) return 0;
+    return map(
+      Math.abs(distanceToCenter_signed()),
+      lineWidthThreshold,
+      rotationThreshold,
+      0,
+      window.innerWidth + 100
+    );
+  };
+
+  const translateX = (): number => {
+    return (x - centerX) * 0.3;
+  };
 
   return (
     <div
-      id="circle"
       onMouseMove={handleMouse}
       className="flex fixed min-w-full min-h-full top-0 left-0 justify-center items-center"
-      >
-          <motion.div>
-            <h1>Hi, I&rsquo;m Tao</h1>
-              
-          </motion.div>
-          
+    >
+      <motion.div>
+        <h1 className="categoryBanner">Architecture</h1>
+      </motion.div>
+
+      {/* center circle */}
+      <div className="fixed w-4 h-4 bg-black rounded-full"></div>
+      {/* Hi Im Tao */}
       <motion.div
-        className="h-1 fixed bg-black"
+        className="fixed top-1/3"
         style={{
-            rotateZ: translateRotation(),
-            width: translateWidth(),
+          x: translateX(),
         }}
       >
-          </motion.div>
-          <motion.div>
-              
-          </motion.div>
-          <p>a computational architect and fullstack developer</p>
+        <h1>Hi, I&rsquo;m Tao</h1>
+      </motion.div>
+      {/* center line */}
+      <motion.div
+        className="h-px fixed bg-black"
+        style={{
+          rotateZ: translateRotation(),
+          width: translateWidth(),
+        }}
+      ></motion.div>
+      {/* tag line */}
+      <motion.div
+        className="fixed bottom-1/3"
+        style={{
+          x: translateX() * -1,
+        }}
+      >
+        <p>a computational architect and fullstack developer</p>
+      </motion.div>
     </div>
   );
 }
