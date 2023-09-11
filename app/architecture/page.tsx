498649a10477c1e3df6323f05e8a901d1ce6062e
Tao <taole.chen@gmail.com>
2023-09-11 08:15:40 +0200

    added relative position function for testing

--- app/architecture/page.tsx
+++ app/architecture/page.tsx
@@ -21,7 +21,11 @@ function Architecture() {
 
 	useEffect(() => {
 		const initAnim = async () => {
-			await Promise.all([animate("#container_projectindex", { width: "50%", padding: "4rem" }, { duration: 1, ease: "easeOut" }), animate("#projects", { x: 0 }, { duration: 1, ease: "easeOut", delay: 1 }), animate("#title_arch", { x: 0 }, { duration: 1, ease: "easeOut", delay: 1 }), animate("#center_circle", { backgroundColor: "#ffffff" }, { duration: 1, ease: "easeOut", delay: 1 })]);
+            await Promise.all([
+                animate("#container_projectindex", { width: "50%", padding: "4rem" }, { duration: 1, ease: "easeOut" }),
+                animate("#projects", { x: 0 }, { duration: 1, ease: "easeOut", delay: 1 }),
+                animate("#title_arch", { x: 0 }, { duration: 1, ease: "easeOut", delay: 1 }),
+                animate("#center_circle", { backgroundColor: "#ffffff" }, { duration: 1, ease: "easeOut", delay: 1 })]);
 		};
 
         initAnim();
@@ -33,16 +37,47 @@ function Architecture() {
     const handleMouse = async (e: MouseEvent) => {
         setX(e.pageX);
         setY(e.pageY);
-        await animate('#center_circle', {y: moveCenterCircle()}, {});
+        // await animate('#center_circle', {y: moveCenterCircle()}, {});
     }
 
     const moveCenterCircle = () => {
-        return y - centerY;
+        // return y - centerY;
     }
 	/**
 	 * fetch project from database with provided id string
 	 */
 	const fetchProject = async () => {};
+
+    function getRelativeCoordinates(event: MouseEvent, referenceElement:any) {
+        const position = {
+          x: event.pageX,
+          y: event.pageY
+        };
+      
+        const offset = {
+          left: referenceElement.offsetLeft,
+          top: referenceElement.offsetTop,
+          width: referenceElement.clientWidth,
+          height: referenceElement.clientHeight
+        };
+      
+        let reference = referenceElement.offsetParent;
+      
+        while (reference) {
+          offset.left += reference.offsetLeft;
+          offset.top += reference.offsetTop;
+          reference = reference.offsetParent;
+        }
+      
+        return {
+          x: position.x - offset.left,
+          y: position.y - offset.top,
+          width: offset.width,
+          height: offset.height,
+          centerX: (position.x - offset.left - offset.width / 2) / (offset.width / 2),
+          centerY: (position.y - offset.top - offset.height / 2) / (offset.height / 2)
+        };
+      }
 
 	return (
 		<main ref={scope} className="flex min-h-screen h-full w-full">
@@ -52,6 +87,7 @@ function Architecture() {
 				className="fixed w-4 h-4 rounded-full inset-x-0 inset-y-0 mx-auto top-1/2 z-10"
 				style={{
                     backgroundColor: "#000000",
+                    y:y
 				}}
 			></motion.div>
 
